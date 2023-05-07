b523f7f31a18735d829f35d76a5c49a601f05f96
keshnath <princeshubham.a@gmail.com>
2023-05-07 14:58:34 +0530

    first

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+import OtpVerification from "./components/OtpVerification";
 
 function App() {
+  const [otp, setOtp] = useState("");
+  const handleOnchange = (e) => {
+    setOtp(e);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="flex justify-center mt-10">
+      <div className="items-center text-center h-96 border w-96" >
+        <h1 className="p-6 text-3xl">Phone Verification</h1>
+        <hr/>
+        
+        <h1 className="py-2 pb-10 text-gray-600">Enter OTP You Recieved On 955473XXXX</h1>
+        <div>
+          <OtpVerification
+            otp={otp}
+            handleOnchange={handleOnchange}
+            valueLength={6}
+          />
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/components/OtpVerification.js
@@ -0,0 +1,87 @@
+import React, { useMemo } from "react";
+import { RE } from "../Helper";
+
+const OtpVerification = ({ otp, handleOnchange, valueLength }) => {
+  const valueItems = useMemo(() => {
+    let items = [];
+    const otpArr = otp.split("");
+    for (let i = 0; i < valueLength; i++) {
+      let char = otpArr[i];
+      if (char) {
+        items.push(char);
+      } else {
+        items.push("");
+      }
+    }
+    return items;
+  }, [otp, valueLength]);
+
+  const inputOnchange = (e, idx) => {
+    const target = e.target;
+    let targetValue = target.value;
+    const isTargetValueDigit = RE.test(targetValue);
+
+    if (!isTargetValueDigit && targetValue !== "") {
+      return;
+    }
+
+    targetValue = isTargetValueDigit ? targetValue : " ";
+
+    const newValue =
+      otp.substring(0, idx) + targetValue + otp.substring(idx + 1);
+
+    handleOnchange(newValue);
+
+    if (!isTargetValueDigit) {
+      return;
+    }
+
+    const nextElementSibling = target.nextElementSibling;
+
+    if (nextElementSibling) {
+      nextElementSibling.focus();
+    }
+  };
+
+  const handleOnKeyPress  = (e)=>{
+    const target = e.target
+    const value = target.value
+    if(!e.key === 'Backspace' || value != " "){
+      return 
+    }
+    const previousElementSibling = target.previousElementSibling 
+    if(previousElementSibling){
+      previousElementSibling.focus()
+    }
+    
+  }
+  const handleOnFocus = (e)=>{
+    
+  }
+
+  return (
+    <div>
+      {valueItems.map((e, ind) => (
+        
+          <input
+          className="border-b border-black w-9 text-center m-2"
+          type="text"
+          value={e}
+          key={ind}
+          maxLength={6}
+          onChange={(e) => {
+            inputOnchange(e, ind);
+          }}
+          onKeyDown={handleOnKeyPress}
+          onFocus={handleOnFocus}
+        />
+        
+        
+      ))}
+      <div className="flex justify-between m-6"><h1 className="text-blue-500">Change Number</h1> <h1 className="text-blue-500">Re-send OTP</h1></div>
+      <div><button className="border border-black rounded-full px-7 py-2 bg-green-300">Verify Phone Number</button></div>
+    </div>
+  );
+};
+
+export default OtpVerification;
